c143a1478bb0c7e3beaae05f101820ba1832478d
huanglong <2350029488@qq.com>
2022-09-04 11:54:44 +0800

    002

--- login-user/src/main/java/com/huanglong/login/entity/User.java
+++ login-user/src/main/java/com/huanglong/login/entity/User.java
@@ -41,14 +41,14 @@ public class User implements Serializable {
         return id;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
 
     public String getUsername() {
         return username;
     }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
     public void setUsername(String username) {
         this.username = username;
     }
